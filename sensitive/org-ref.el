fb9eec77792f1ec5876b1102e92fca46e8b9a67a
Lucas Vieira <lucasvieira@protonmail.com>
2020-04-30 22:01:52 -0300

    Many changes, see more.
    - Add Orderless completion
    - Add summary
    - Change theme to light theme
    - Reconfigure org-noter
    - Change org-roam so it uses MELPA
    - Add org-roam-bibtex
    - Add org-toc
    - Add poly-org (commented out)
    - Add prettify-symbols and fira-code to Lisp buffers
    - Comment out Firefox
    - Disable EXWM properly
    - Add writeroom-mode

--- sensitive/org-ref.el
+++ sensitive/org-ref.el
@@ -4,3 +4,4 @@
 ;; org-ref-default-bibliography
 ;; Both these variables must be the path for your default .bib file.
 
+;; Setq the variables reftex-default-bibliography and org-ref-default-bibliography.
